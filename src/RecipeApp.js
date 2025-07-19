e8505a6d229b3d177b0e8d97a4786fb057d3d0e9
Rockoholics <rockwanyan@gmail.com>
2025-07-19 21:22:12 +1000

    Add files via upload

---
+++ src/RecipeApp.js
@@ -0,0 +1,1 @@
+export default function RecipeApp() { return <div>Loading...</div> }
---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import RecipeApp from './RecipeApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RecipeApp />);
