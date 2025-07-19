d1a326733c5b658fd2e2f0fb12083aa310c5a4a3
Rockoholics <rockwanyan@gmail.com>
2025-07-19 21:41:09 +1000

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import RecipeApp from './RecipeApp';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import RecipeApp from "./RecipeApp";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RecipeApp />);
